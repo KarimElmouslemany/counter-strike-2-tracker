0255934ab3dc6aee9640f4263c64269ab2cc1296
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-05-28 03:58:40 +0100

    the layout for the app

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
-import {Stack } from 'expo-router'
+import { Tabs } from 'expo-router'
 import React from 'react'
-
+import { colors } from "../color_Assests/Color";
 const _layout = () => {
   return (
-    <Stack/>
+    <Tabs>
+      <Tabs.Screen name='index' options={{headerShown: false}}/>
+      <Tabs.Screen/>
+    </Tabs>
   )
 }
 
