da06d3c9ecf196db3582e95a3b00082cd6208d52
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-05-28 03:54:49 +0100

    made the basic design of the front of the app  where all of the players main information will go

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import React from "react";
 import { Link } from "expo-router";
+import { colors } from "../color_Assests/Color";
 const Home = () => {
   return (
-    <View style={styles.Container}>
-      <Text>Home</Text>
-     <Link href="/Statues">Stautes</Link>
-    </View>
-  )
-}
+    <ScrollView>
+      <View style={styles.Container}>
+        <View style={styles.innerContainerRanks}>
+          <View style={styles.ContainerCurrentRank}>
+            <Text>Current Rank</Text>
+          </View>
+          <View style={styles.ContainerCurrentPeak}>
+            <Text>Peak Rank</Text>
+          </View>
+        </View>
+        <View style={styles.ContainerPlayerInfo}>
+          <Text style={styles.boldText}>Headshot %</Text>
+          <Text style={styles.boldText}>K/D Ratio</Text>
+          <Text style={styles.boldText}>Win %</Text>
+        </View>
+        <View style={styles.ContainerWeponeInfo}>
+          <Text>Weapons</Text>
+          <FlatList></FlatList>
+        </View>
+        <View style={styles.ContainerMapsInfo}>
+          <Text>Maps</Text>
+          <FlatList></FlatList>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-   Container:{
+  Container: {
     flex: 1,
+    backgroundColor: colors.background,
+  },
+  Title: {
+    fontSize: 22,
+    alignContent: "center",
+    color: colors.textPrimary,
+  },
+  innerContainerRanks: {
+    backgroundColor: colors.card,
+    height: 200,
+    marginTop: 100,
     alignItems: "center",
-    justifyContent:"center"
-  }
-})
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  ContainerCurrentRank: {
+    backgroundColor: "white",
+    width: 150,
+    padding: 12,
+    borderRadius: 8,
+  },
+  ContainerCurrentPeak: {
+    backgroundColor: "red",
+    width: 150,
+    borderRadius: 8,
+    padding: 12,
+  },
+  ContainerPlayerInfo: {
+    backgroundColor: "white",
+    height: 150,
+    marginTop: 100,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  ContainerWeponeInfo: {
+    backgroundColor: colors.card,
+    height: 150,
+    marginTop: 100,
+  },
+  ContainerMapsInfo: {
+    backgroundColor: colors.card,
+    height: 150,
+    marginTop: 100,
+  },
+  boldText: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
 // rnfes
