99f480f18db64c932711248006a37d5afa4e50e7
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-06-03 20:51:58 +0100

    added a object array for all the wepones name and ID so that the ID can be used to search for the wepones in the API.Futheremore  the functiion retrieving_wepones will loop through the wepoenlist object and thee API to check the iid of each wepoens and get the kills,hits,shots for each wepone and it to playinfo as a object array. Also main now awaits for the getuserinfo function so that it can be passed to the other functions istead of each function calling the API.

--- API_calls/steam.js
+++ API_calls/steam.js
@@ -2,7 +2,30 @@ const API_Key = "0398A494A62EAB4D439E67759FF16A1E";
 const url = `https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?appid=730&key=${API_Key}&steamid=76561199043122406`;
 let totalhits = 0;
 let total_shots_fired = 0;
-
+const weaponList = [
+  { id: "ak47", name: "AK-47" },
+  { id: "m4a1", name: "M4A1" },
+  { id: "awp", name: "AWP" },
+  { id: "deagle", name: "Desert Eagle" },
+  { id: "galilar", name: "Galil AR" },
+  { id: "famas", name: "FAMAS" },
+  { id: "aug", name: "AUG" },
+  { id: "sg556", name: "SG 553" },
+  { id: "ssg08", name: "SSG 08" },
+  { id: "glock", name: "Glock-18" },
+  { id: "hkp2000", name: "P2000" },
+  { id: "p250", name: "P250" },
+  { id: "fiveseven", name: "Five-SeveN" },
+  { id: "tec9", name: "Tec-9" },
+  { id: "elite", name: "Dual Berettas" },
+  { id: "mp9", name: "MP9" },
+  { id: "mp7", name: "MP7" },
+  { id: "p90", name: "P90" },
+  { id: "mac10", name: "MAC-10" },
+  { id: "nova", name: "Nova" },
+  { id: "xm1014", name: "XM1014" },
+  { id: "knife", name: "Knife" },
+];
 const playerInfo = {
   overview: {
     kills: 0,
@@ -26,8 +49,8 @@ const playerInfo = {
     headshotKills: 0,
     flashedKills: 0,
   },
-  weapons: [{ id: 0, name: 0, kills: 0, shots: 0, hits: 0, accuracy: 0 }],
-  maps: [{ id: 0, name: 0, wins: 0, rounds: 0, winRate: 0 }],
+  weapons: [],
+  maps: [],
   meta: {
     steamId: 0,
     fetchedAt: 0,
@@ -45,10 +68,8 @@ async function getuserInfo(API_Key, url) {
   }
   return Player_info;
 }
-async function user_info_simple_display() {
-  const user_info = await getuserInfo(API_Key, url);
+function user_info_simple_display(user_info) {
   for (s of user_info) {
-    // console.log(s.name, s.value);
     if (s.name === "total_shots_fired") {
       total_shots_fired = s.value;
     }
@@ -57,6 +78,7 @@ async function user_info_simple_display() {
     }
     if (s.name === "total_kills") {
       playerInfo.overview.kills = s.value;
+      totalKills = s.value;
     }
     if (s.name === "total_deaths") {
       playerInfo.overview.deaths = s.value;
@@ -108,15 +130,43 @@ function metrics_needed_for_calculation(totalhits, shots_fired) {
   playerInfo.overview.headshotPercent =
     (playerInfo.combat.headshotKills / playerInfo.overview.kills) * 100;
   playerInfo.overview.accuracy = (totalhits / total_shots_fired) * 100;
-
-  // console.log("this is the total hiites:",totalhits);
-  // console.log("this is the total shots:",total_shots_fired);
 }
-async function retrieving_weapons() {
-  const user_info = await getuserInfo(API_Key, url);
-  for(w of user_info){
-      
+function retrieving_weapons(user_info) {
+  let t_kills = 0;
+  let t_hits = 0;
+  let t_shots = 0;
+  let weapon_accuracy = 0;
+
+  for (const n of weaponList) {
+    for (const w of user_info) {
+      if (w.name.includes(`total_shots_${n.id}`)) {
+        t_shots = w.value;
+      }
+      if (w.name.includes(`total_kills_${n.id}`)) {
+        t_kills = w.value;
+      }
+      if (w.name.includes(`total_hits_${n.id}`)) {
+        t_hits = w.value;
+      }
+      weapon_accuracy = (t_hits / t_shots) * 100;
+      killShare = (t_kills / playerInfo.overview.kills) * 100;
+    }
+    playerInfo.weapons.push({
+      id: n.id,
+      name: n.name,
+      Kills: t_kills,
+      hits: t_hits,
+      shots: t_shots,
+      accuracy: weapon_accuracy,
+      KillShare: killShare,
+    });
   }
-}
-user_info_simple_display();
 
+  console.log(playerInfo);
+}
+async function main() {
+  const user_info = await getuserInfo(API_Key, url);
+  user_info_simple_display(user_info);
+  retrieving_weapons(user_info);
+}
+main();
