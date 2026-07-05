887dfdd09fc21598a90cc12be71d779396ecfdb4
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-07-05 22:26:45 +0100

    got ride of the header changed the nav bar background and the colors when the nav bar is active and inactive.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
 import { Tabs } from 'expo-router'
 import React from 'react'
 import { colors } from "../color_Assests/Color";
 const _layout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name='index' options={{headerShown: false}}/>
-      <Tabs.Screen/>
+    <Tabs screenOptions={{headerShown: false, 
+      tabBarStyle:{
+        backgroundColor: colors.navBackground,
+
+      },
+      tabBarActiveTintColor: colors.navActive,
+      tabBarInactiveTintColor: colors.navInactive
+    
+    }}>
     </Tabs>
   )
 }
