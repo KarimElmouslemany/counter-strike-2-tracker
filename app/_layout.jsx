7ce2a5df2a08c922056f1fb57b1035206b25dae7
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-07-12 22:21:41 +0100

    got ride of the header and added colour to the nav bar. in addtion added color for when the nav bar is active and iactive

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,23 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
+import { Tabs } from "expo-router";
+import React from "react";
 import { colors } from "../color_Assests/Color";
+import { StatusBar } from "expo-status-bar";
 const _layout = () => {
   return (
-    <Tabs screenOptions={{headerShown: false, 
-      tabBarStyle:{
-        backgroundColor: colors.navBackground,
+    <>
+      <StatusBar style="light" backgroundColor={colors.background} />
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: colors.navBackground,
+          },
+          tabBarActiveTintColor: colors.navActive,
+          tabBarInactiveTintColor: colors.navInactive,
+        }}
+      ></Tabs>
+    </>
+  );
+};
 
-      },
-      tabBarActiveTintColor: colors.navActive,
-      tabBarInactiveTintColor: colors.navInactive
-    
-    }}>
-    </Tabs>
-  )
-}
-
-export default _layout
-
+export default _layout;
