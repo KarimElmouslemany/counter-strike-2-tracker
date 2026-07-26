bdd9badfff47e74688271fb9663a889f49c1bed8
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-07-26 20:41:26 +0100

    created the input page with the function sendUserInfo taking users steamID that gets triggered

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {checkSteamID } from "../API_calls/steam";
 
 const InputPage = () => {
+  const [SteamID, setSteamID] = useState("");
+  const [errormessage, seterrors] = useState("");
+  async function sendUserInfo(ID) {
+    const checkResult = checkSteamID(ID);
+    seterrors(checkResult.message);
+  }
   return (
-    <View>
-      <Text>InputPage</Text>
+    <View style={styles.Container}>
+      <TextInput
+        style={styles.inputstyling}
+        value={SteamID}
+        onChangeText={setSteamID}
+      ></TextInput>
+
+      <Button title="Send" onPress={() => sendUserInfo(SteamID)}></Button>
+      <Text style={styles.text}>{errormessage}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default InputPage
+export default InputPage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+  },
+  inputstyling: {
+    height: 40,
+    width: 200,
+    padding: 10,
+    borderWidth: 1,
+    margin: 100,
+  },
+  text: {
+    margin: 50,
+    padding: 10,
+    fontSize: 30,
+    color: "red",
+  },
+  stylingButtton: {},
+});
